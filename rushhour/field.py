b0a544484297707aa3851cb29cc2837565b9d8c7
Jesse Bouma <j.w.bouma@gmail.com>
2014-11-09 22:28:29 +0100

    Field now holds vehicles, instead of vehicle holding field.

--- rushhour/field.py
+++ rushhour/field.py
@@ -1,6 +1,10 @@
 class Field(object):
-    def __init__(self, length):
+    def __init__(self, length, vehicles):
         self.tiles = [[0 for x in range(length)] for x in range(length)]
+        self.vehicles = vehicles
+
+        for vehicle in self.vehicles:
+            vehicle.insertVehicleInField(self)
 
     def modifyTile(self, pos, status):
         if status == "occupy":
@@ -10,7 +14,7 @@ class Field(object):
 
     def isTileEmpty(self, pos):
         try:
-            if self.tiles[pos[1] - 1][pos[0] - 1] == 1:
+            if self.tiles[pos[1] - 1][pos[0] - 1] == 1 or pos[0] < 1 or pos[1] < 1:
                 return False
         except IndexError:
             return False
@@ -27,42 +31,42 @@ class Field(object):
 
 
 class Vehicle(object):
-    def __init__(self, field, positions, orientation, color):
-        self.field = field
+    def __init__(self, positions, orientation, color):
         self.positions = positions
         self.orientation = orientation
         self.color = color
 
+    def insertVehicleInField(self, field):
         for pos in self.positions:
-            self.field.modifyTile(pos, "occupy")
+            field.modifyTile(pos, "occupy")
 
-    def getPossibleMoves(self):
+    def getPossibleMoves(self, field):
         possibleMoves = []
         if self.orientation == "horizontal":
             leftTile = (self.positions[0][0] - 1, self.positions[0][1])
-            if self.field.isTileEmpty(leftTile):
+            if field.isTileEmpty(leftTile):
                 tileToLeave = self.positions[-1]
                 possibleMoves.append((tileToLeave, leftTile, "left"))
             rightTile = (self.positions[-1][0] + 1, self.positions[-1][1])
-            if self.field.isTileEmpty(rightTile):
+            if field.isTileEmpty(rightTile):
                 tileToLeave = self.positions[0]
                 possibleMoves.append((tileToLeave, rightTile, "right"))
 
         if self.orientation == "vertical":
             upTile = (self.positions[0][0], self.positions[0][1] - 1)
-            if self.field.isTileEmpty(upTile):
+            if field.isTileEmpty(upTile):
                 tileToLeave = self.positions[-1]
                 possibleMoves.append((tileToLeave, upTile, "up"))
             downTile = (self.positions[-1][0], self.positions[-1][1] + 1)
-            if self.field.isTileEmpty(downTile):
+            if field.isTileEmpty(downTile):
                 tileToLeave = self.positions[0]
                 possibleMoves.append((tileToLeave, downTile, "down"))
 
         return possibleMoves
 
-    def move(self, tileToLeave, tileToOccupy, direction):
-        self.field.modifyTile(tileToLeave, "leave")
-        self.field.modifyTile(tileToOccupy, "occupy")
+    def move(self, field, tileToLeave, tileToOccupy, direction):
+        field.modifyTile(tileToLeave, "leave")
+        field.modifyTile(tileToOccupy, "occupy")
 
         self.positions.remove(tileToLeave)
         if direction == "left" or direction == "up":
@@ -72,15 +76,15 @@ class Vehicle(object):
 
 
 if __name__ == "__main__":
-    field = Field(6)
-    field2 = Field(6)
-    print field == field2
+    v1 = Vehicle([(1, 1), (1, 2), (1, 3)], "vertical", "blue")
+    v2 = Vehicle([(4, 3), (5, 3), (6, 3)], "horizontal", "yellow")
+    v3 = Vehicle([(2, 6), (3, 6)], "horizontal", "green")
+    v4 = Vehicle([(4, 5), (4, 6)], "vertical", "orange")
+    vehicles = [v1, v2, v3, v4]
+    field = Field(6, vehicles)
     print field
-    v1 = Vehicle(field, [(1, 1), (1, 2), (1, 3)], "vertical", "blue")
-    v2 = Vehicle(field, [(4, 3), (5, 3), (6, 3)], "horizontal", "yellow")
-    v3 = Vehicle(field, [(2, 6), (3, 6)], "horizontal", "green")
-    print field
-    v2Moves = v2.getPossibleMoves()
-    for move in v2Moves:
-        v2.move(move[0], move[1], move[2])
+    for vehicle in field.vehicles:
+        moves = vehicle.getPossibleMoves(field)
+        for move in moves:
+            vehicle.move(field, move[0], move[1], move[2])
     print field
