bc302bb2600847a54c5e79a47b8d8df9af2d846b
Jesse Bouma <j.w.bouma@gmail.com>
2014-11-16 12:38:11 +0100

    Correct vehicle positions.

--- rushhour/games.py
+++ rushhour/games.py
@@ -1,100 +1,63 @@
 from field import Field, Vehicle
-from rushvisual import *
+# from rushvisual import *
 
 def field1():
-    # v1 = Vehicle([(1, 5), (1, 6)], "vertical", "orange")
-    # v2 = Vehicle([(2, 5), (3, 5)], "horizontal", "blue")
-    # v3 = Vehicle([(3, 1), (3, 2), (3, 3)], "vertical", "purple")
-    # v4 = Vehicle([(4, 4), (4, 5), (4, 6)], "vertical", "orange")
-    # v5 = Vehicle([(5, 6), (6, 6)], "horizontal", "green")
-    # v6 = Vehicle([(5, 4), (6, 4)], "horizontal", "orange")
-    # v7 = Vehicle([(4, 1), (5, 1)], "horizontal", "blue")
-    # v8 = Vehicle([(6, 1), (6, 2), (6, 3)], "vertical", "yellow")
-    # red = Vehicle([(4, 3), (5, 3)], "horizontal", "red")
-    v1 = Vehicle([(1, 2), (1, 1)], "vertical", "orange")
-    v2 = Vehicle([(2, 2), (3, 2)], "horizontal", "blue")
-    v3 = Vehicle([(3, 6), (3, 5), (3, 4)], "vertical", "purple")
-    v4 = Vehicle([(4, 3), (4, 2), (4, 1)], "vertical", "orange")
-    v5 = Vehicle([(5, 1), (6, 1)], "horizontal", "green")
-    v6 = Vehicle([(5, 3), (6, 3)], "horizontal", "orange")
-    v7 = Vehicle([(4, 6), (5, 6)], "horizontal", "blue")
-    v8 = Vehicle([(6, 6), (6, 5), (6, 4)], "vertical", "yellow")
-    red = Vehicle([(4, 4), (5, 4)], "horizontal", "red")
+    v1 = Vehicle([(1, 5), (1, 6)], "vertical", "orange")
+    v2 = Vehicle([(2, 5), (3, 5)], "horizontal", "blue")
+    v3 = Vehicle([(3, 1), (3, 2), (3, 3)], "vertical", "purple")
+    v4 = Vehicle([(4, 4), (4, 5), (4, 6)], "vertical", "orange")
+    v5 = Vehicle([(5, 6), (6, 6)], "horizontal", "green")
+    v6 = Vehicle([(5, 4), (6, 4)], "horizontal", "orange")
+    v7 = Vehicle([(4, 1), (5, 1)], "horizontal", "blue")
+    v8 = Vehicle([(6, 1), (6, 2), (6, 3)], "vertical", "yellow")
+    red = Vehicle([(4, 3), (5, 3)], "horizontal", "red")
 
     vehicles = [v1, v2, v3, v4, v5, v6, v7, v8, red]
     field = Field(6, vehicles)
-    anim = RushVisualisation(6, 6, vehicles)
-    print anim
+    # anim = RushVisualisation(6, 6, vehicles)
+    # print anim
     return field
 
 def field2():
-    # v1 = Vehicle([(1, 5), (1, 6)], "vertical", "orange")
-    # v2 = Vehicle([(1, 4), (2, 4)], "horizontal", "green")
-    # v3 = Vehicle([(3, 4), (4, 4)], "horizontal", "blue")
-    # v4 = Vehicle([(4, 5), (4, 6)], "vertical", "blue") # lightblue
-    # v5 = Vehicle([(5, 6), (6, 6)], "horizontal", "orange")
-    # v6 = Vehicle([(5, 5), (6, 5)], "horizontal", "green")
-    # v7 = Vehicle([(5, 3), (5, 4)], "vertical", "blue") # lightblue
-    # v8 = Vehicle([(2, 2), (3, 2)], "horizontal", "orange")
-    # v9 = Vehicle([(4, 2), (5, 2)], "horizontal", "green")
-    # v10 = Vehicle([(6, 2), (6, 3), (6, 4)], "vertical", "yellow")
-    # v11 = Vehicle([(3, 1), (4, 1)], "horizontal", "blue")
-    # v12 = Vehicle([(5, 1), (6, 1)], "horizontal", "orange")
-    # red = Vehicle([(3, 3), (4, 3)], "horizontal", "red")
-
-    v1 = Vehicle([(1, 2), (1, 1)], "vertical", "orange")
-    v2 = Vehicle([(1, 3), (2, 3)], "horizontal", "green")
-    v3 = Vehicle([(3, 3), (4, 3)], "horizontal", "blue")
-    v4 = Vehicle([(4, 2), (4, 1)], "vertical", "blue") # lightblue
-    v5 = Vehicle([(5, 1), (6, 1)], "horizontal", "orange")
-    v6 = Vehicle([(5, 2), (6, 2)], "horizontal", "green")
-    v7 = Vehicle([(5, 4), (5, 3)], "vertical", "blue") # lightblue
-    v8 = Vehicle([(2, 5), (3, 5)], "horizontal", "orange")
-    v9 = Vehicle([(4, 5), (5, 5)], "horizontal", "green")
-    v10 = Vehicle([(6, 5), (6, 4), (6, 3)], "vertical", "yellow")
-    v11 = Vehicle([(3, 6), (4, 6)], "horizontal", "blue")
-    v12 = Vehicle([(5, 6), (6, 6)], "horizontal", "orange")
-    red = Vehicle([(3, 4), (4, 4)], "horizontal", "red")
+    v1 = Vehicle([(1, 5), (1, 6)], "vertical", "orange")
+    v2 = Vehicle([(1, 4), (2, 4)], "horizontal", "green")
+    v3 = Vehicle([(3, 4), (4, 4)], "horizontal", "blue")
+    v4 = Vehicle([(4, 5), (4, 6)], "vertical", "blue") # lightblue
+    v5 = Vehicle([(5, 6), (6, 6)], "horizontal", "orange")
+    v6 = Vehicle([(5, 5), (6, 5)], "horizontal", "green")
+    v7 = Vehicle([(5, 3), (5, 4)], "vertical", "blue") # lightblue
+    v8 = Vehicle([(2, 2), (3, 2)], "horizontal", "orange")
+    v9 = Vehicle([(4, 2), (5, 2)], "horizontal", "green")
+    v10 = Vehicle([(6, 2), (6, 3), (6, 4)], "vertical", "yellow")
+    v11 = Vehicle([(3, 1), (4, 1)], "horizontal", "blue")
+    v12 = Vehicle([(5, 1), (6, 1)], "horizontal", "orange")
+    red = Vehicle([(3, 3), (4, 3)], "horizontal", "red")
 
     vehicles = [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, red]
     field = Field(6, vehicles)
-    anim = RushVisualisation(6, 6, vehicles)
-    print anim
+    # anim = RushVisualisation(6, 6, vehicles)
+    # print anim
     return field
 
 def field3():
-    # v1 = Vehicle([(2, 1), (3, 1)], "horizontal", "blue")
-    # v2 = Vehicle([(4, 1), (5, 1), (6, 1)], "horizontal", "yellow")
-    # v3 = Vehicle([(2, 2), (3, 2)], "horizontal", "orange")
-    # v4 = Vehicle([(4, 2), (4, 3)], "vertical", "blue")
-    # v5 = Vehicle([(5, 2), (6, 2)], "horizontal", "green")
-    # v6 = Vehicle([(3, 3), (3, 4)], "vertical", "blue") # lightblue
-    # v7 = Vehicle([(1, 4), (2, 4)], "horizontal", "green")
-    # v8 = Vehicle([(4, 4), (5, 4)], "horizontal", "blue")
-    # v9 = Vehicle([(6, 3), (6, 4)], "vertical", "blue") # lightblue
-    # v10 = Vehicle([(1, 5), (1, 6)], "vertical", "orange")
-    # v11 = Vehicle([(3, 5), (3, 6)], "vertical", "green")
-    # v12 = Vehicle([(5, 5), (6, 5)], "horizontal", "green")
-    # red = Vehicle([(1, 3), (2, 3)], "horizontal", "red")
-
-    v1 = Vehicle([(2, 6), (3, 6)], "horizontal", "blue")
-    v2 = Vehicle([(4, 6), (5, 6), (6, 6)], "horizontal", "yellow")
-    v3 = Vehicle([(2, 5), (3, 5)], "horizontal", "orange")
-    v4 = Vehicle([(4, 5), (4, 4)], "vertical", "blue")
-    v5 = Vehicle([(5, 5), (6, 5)], "horizontal", "green")
-    v6 = Vehicle([(3, 4), (3, 3)], "vertical", "blue") # lightblue
-    v7 = Vehicle([(1, 3), (2, 3)], "horizontal", "green")
-    v8 = Vehicle([(4, 3), (5, 3)], "horizontal", "blue")
-    v9 = Vehicle([(6, 4), (6, 3)], "vertical", "blue") # lightblue
-    v10 = Vehicle([(1, 2), (1, 1)], "vertical", "orange")
-    v11 = Vehicle([(3, 2), (3, 1)], "vertical", "green")
-    v12 = Vehicle([(5, 2), (6, 2)], "horizontal", "green")
-    red = Vehicle([(1, 4), (2, 4)], "horizontal", "red")
+    v1 = Vehicle([(2, 1), (3, 1)], "horizontal", "blue")
+    v2 = Vehicle([(4, 1), (5, 1), (6, 1)], "horizontal", "yellow")
+    v3 = Vehicle([(2, 2), (3, 2)], "horizontal", "orange")
+    v4 = Vehicle([(4, 2), (4, 3)], "vertical", "blue")
+    v5 = Vehicle([(5, 2), (6, 2)], "horizontal", "green")
+    v6 = Vehicle([(3, 3), (3, 4)], "vertical", "blue") # lightblue
+    v7 = Vehicle([(1, 4), (2, 4)], "horizontal", "green")
+    v8 = Vehicle([(4, 4), (5, 4)], "horizontal", "blue")
+    v9 = Vehicle([(6, 3), (6, 4)], "vertical", "blue") # lightblue
+    v10 = Vehicle([(1, 5), (1, 6)], "vertical", "orange")
+    v11 = Vehicle([(3, 5), (3, 6)], "vertical", "green")
+    v12 = Vehicle([(5, 5), (6, 5)], "horizontal", "green")
+    red = Vehicle([(1, 3), (2, 3)], "horizontal", "red")
 
     vehicles = [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, red]
     field = Field(6, vehicles)
-    anim = RushVisualisation(6, 6, vehicles)
-    print anim
+    # anim = RushVisualisation(6, 6, vehicles)
+    # print anim
     return field
 
 if __name__ == "__main__":
--- rushhour/rushhour.py
+++ rushhour/rushhour.py
@@ -1,7 +1,7 @@
 import copy
+import games
 import Queue
 
-from rushvisual import *
 from field import Field, Vehicle
 
 
@@ -9,8 +9,6 @@ def BFSearch(fieldsQueue, visited):
     solution = None
     while solution is None:
         field = fieldsQueue.get()
-        # if field.tiles == [[0, 0, 0, 1, 1, 1], [0, 0, 1, 0, 0, 1], [0, 0, 1, 1, 1, 1], [0, 0, 1, 1, 1, 1], [1, 1, 1, 1, 0, 0], [1, 0, 0, 1, 1, 1]]:
-#             print field
         for x in range(len(field.vehicles)):
             vehicle = field.vehicles[x]
             moves = vehicle.getPossibleMoves(field)
@@ -29,28 +27,7 @@ def BFSearch(fieldsQueue, visited):
     return solution
 
 if __name__ == "__main__":
-    # v1 = Vehicle([(2, 1), (3, 1)], "horizontal", "blue")
-    # v2 = Vehicle([(4, 1), (5, 1), (6, 1)], "horizontal", "yellow")
-    # v3 = Vehicle([(2, 2), (3, 2)], "horizontal", "orange")
-    # v4 = Vehicle([(4, 2), (4, 3)], "vertical", "blue")
-    # v5 = Vehicle([(5, 2), (6, 2)], "horizontal", "green")
-    # v6 = Vehicle([(3, 3), (3, 4)], "vertical", "blue") # lightblue
-    # v7 = Vehicle([(1, 4), (2, 4)], "horizontal", "green")
-    # v8 = Vehicle([(4, 4), (5, 4)], "horizontal", "blue")
-    # v9 = Vehicle([(6, 3), (6, 4)], "vertical", "blue") # lightblue
-    # v10 = Vehicle([(1, 5), (1, 6)], "vertical", "orange")
-    # v11 = Vehicle([(3, 5), (3, 6)], "vertical", "green")
-    # v12 = Vehicle([(5, 5), (6, 5)], "horizontal", "green")
-    # red = Vehicle([(1, 3), (2, 3)], "horizontal", "red")
-    # vehicles = [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, red]
-
-    # field = Field(6, vehicles)
-    # print field
-
+    field = games.field2()
     queue = Queue.Queue()
     queue.put(field)
     BFSearch(queue, set([field]))
-    print field2()
-#     print field3()
-
-    
