69abead90766ea2cb1e510651880ff99ddd922df
Jesse Bouma <j.w.bouma@gmail.com>
2014-11-18 15:47:24 +0100

    Now shows path to solution.

--- rushhour/rushhour.py
+++ rushhour/rushhour.py
@@ -6,14 +6,13 @@ from field import Field, Vehicle
 from rushvisual import RushVisualisation
 
 
-def BFSearch(fieldsQueue, visited, vehicles):
+def BFSearch(start, fieldsQueue, visited, vehicles):
+    parent = {}
     solution = None
     while solution is None:
         field = fieldsQueue.get()
         if field.tiles[field.exit] == "R":
-            print "FOUND SOLUTION"
-            print field
-            solution = field
+            solution = backtrace(parent, start, field)
         movedVehicles = ""
         for tile in field.tiles:
             if tile != "0" and tile not in movedVehicles:
@@ -21,16 +20,26 @@ def BFSearch(fieldsQueue, visited, vehicles):
                 moves = vehicle.getPossibleMoves(field)
                 for move in moves:
                     if not move.tiles in visited:
+                        parent[move] = field
                         visited.add(move.tiles)
                         fieldsQueue.put(move)
                 movedVehicles += tile
     return solution
 
+def backtrace(parent, start, end):
+    path = [end]
+    while path[-1] != start:
+        path.append(parent[path[-1]])
+    path.reverse()
+    return path
+
 if __name__ == "__main__":
-    field = games.field3()
-    vehicles = games.vehicles3()
+    field = games.field2()
+    vehicles = games.vehicles2()
     # RushVisualisation(6, vehicles.values(), field)
     queue = Queue.Queue()
     queue.put(field)
-    a = BFSearch(queue, set([field]), vehicles)
+    a = BFSearch(field, queue, set([field]), vehicles)
+    for state in a:
+        print state
     #RushVisualisation(6, vehicles.values(), a)
