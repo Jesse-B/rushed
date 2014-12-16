97b1ae858abab92bdc79467ff87eb522e8385318
MarcSelles <mselles94@gmail.com>
2014-12-16 02:37:06 +0100

    if statement back

--- rushhour/rushhour.py
+++ rushhour/rushhour.py
@@ -99,7 +99,8 @@ def warmUpForPuzzel(alg, fieldFunc, algType):
 
 def startUp(alg, field, algType, lenght, vehicles, canvas, the_object):
     theVis = RushVisualisation(lenght, vehicles, field().getCars(), canvas, the_object)
-    warmUpForPuzzel(alg, field, algType)
+    if algType == "BF":
+        warmUpForPuzzel(alg, field, algType)
     button5 = Button(the_object, text="Start algoritm", command=lambda: theVis.run(runAlgorithmOnField(alg, field, algType), button5))
     button5.grid(row=0, column=3, sticky=S, pady=15)
 
